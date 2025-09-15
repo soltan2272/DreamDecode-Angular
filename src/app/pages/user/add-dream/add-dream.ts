bec98c79b252408802f21849e5460b6c96b8e21a
Alaa-Mohamed988 <allaa.mohamed9002@gmail.com>
2025-09-15 16:28:24 +0300

    InitialCreate

---
+++ src/app/pages/user/add-dream/add-dream.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-dream',
+  imports: [],
+  templateUrl: './add-dream.html',
+  styleUrl: './add-dream.css'
+})
+export class AddDream {
+
+}
---
+++ src/app/pages/user/get-user-dreams/get-user-dreams.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-user-dreams',
+  imports: [],
+  templateUrl: './get-user-dreams.html',
+  styleUrl: './get-user-dreams.css'
+})
+export class GetUserDreams {
+
+}
